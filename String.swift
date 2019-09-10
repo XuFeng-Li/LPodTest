94f87e0a7a7e78ff64f5a84ba353aac412f2f563
李旭峰 <lxf@asman.com.cn>
2019-09-10 16:27:14 +0800

    extension String

---
+++ String.swift
@@ -0,0 +1,13 @@
+//
+//  String.swift
+//  LPodTest
+//
+//  Created by 李旭峰 on 9/10/19.
+//
+
+import Foundation
+extension String{
+    public func originalLowercased() -> String {
+        return self.lowercased()
+    }
+}
